e80d0c8d22b52a2a873da741f061d3c8467852b6
bungem23 <bungem@udesa.edu.ar>
2026-03-30 16:28:48 -0300

    Header

---
+++ src/screens/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header/Header'
+
+class Results extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header/>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Results;
