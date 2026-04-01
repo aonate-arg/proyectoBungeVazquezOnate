2c0f49bc76655317b132e29f0c67eae9e37f9324
bungem23 <bungem@udesa.edu.ar>
2026-03-31 22:11:21 -0300

    browser

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import Header from "./components/Header/Header"
 
 function App() {
   return (
-  <React.Fragment>
-    <Header/>
-
-  </React.Fragment>
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
   );
 }
 
