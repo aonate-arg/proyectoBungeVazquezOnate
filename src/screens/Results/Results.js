8e42416fbdbd33ef70992122eb24bc2a42b61d9e
bungem23 <bungem@udesa.edu.ar>
2026-04-09 14:46:20 -0300

    results

--- src/screens/Results/Results.js
+++ src/screens/Results/Results.js
@@ -3,7 +3,24 @@ import Header from '../../components/Header/Header';
 import Feader from '../../components/Footer/Footer';
 import Footer from '../../components/Footer/Footer';
 
+const API = "b4012469dde0367276c9701f8ecc44fe"
 class Results extends Component {
+
+  componentDidMount(){
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + API)
+
+            .then(response => response.json())
+            .then(data => this.setState({ datos: data.results }))
+            .catch(error => console.log(error));
+    
+    let busquedaString = localStorage.getItem('Search');
+    let busqueda = JSON.parse(busquedaString);
+
+    
+  }
+
+
+  
   render() {
     return (
       <React.Fragment>
