3d9bb97ff908ce8a256e52ddcbcaa16687f9cbd2
mpvazquez <mpvazquez@udesa.edu.ar>
2026-04-15 09:04:55 -0300

    arreglos en el buscador

--- src/screens/Results/Results.js
+++ src/screens/Results/Results.js
@@ -15,27 +15,43 @@ class Results extends Component {
   }
 
   componentDidMount(){
-    const busqueda = this.props.match.params.id
-    console.log(busqueda)
-    fetch(`https://api.themoviedb.org/3/search/multi?api_key=` + API +`&query=${busqueda}`)
+    const busqueda = this.props.match.params.buscado
+    const mediaType = this.props.match.params.tipo
+
+  if(mediaType=="pelicula"){
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API}&query=${busqueda}`)
       
             .then(response => response.json())
             .then(data => this.setState({ datos: data.results }))
-            .catch(error => console.log(error));
-     
-    }
+            .catch(error => console.log(error)) }
+
+    else{
+    fetch(`https://api.themoviedb.org/3/search/tv?api_key=` + API +`&query=${busqueda}`)
+      
+            .then(response => response.json())
+            .then(data => this.setState({ datos: data.results }))
+            .catch(error => console.log(error)) 
+    }}
 
   render() {
+    const busqueda = this.props.match.params.buscado
+    const mediaType = this.props.match.params.tipo
     return (
+      
       <React.Fragment>
         <Header/>
+         <h2 className="alert alert-warning">Resultados</h2>
         {this.state.datos.map((pelicula,idx)=> (
                         <Card 
                             key= {pelicula+idx}
-                            titulo={this.state.datos.media_type=="movie"?pelicula.name: pelicula.title}
+                            titulo={mediaType=="pelicula"?pelicula.title: pelicula.name}
                             id={pelicula.id}
-                            imagen={pelicula.poster_path}
+                            imagen={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
+                            type= {mediaType=="pelicula"? "movie":"serie"
+                            }
+                           
                             descripcion={pelicula.overview} />))}
+    
         <Footer/>
       </React.Fragment>
     )
@@ -43,3 +59,4 @@ class Results extends Component {
 }
 
 export default Results;
+
