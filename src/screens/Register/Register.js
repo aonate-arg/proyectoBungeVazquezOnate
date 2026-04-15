661dc98069f62149e1985d87d766b27b6edca818
aonate-arg <aonate@udesa.edu.ar>
2026-04-15 10:23:38 -0300

    pequeños arreglos

--- src/screens/Register/Register.js
+++ src/screens/Register/Register.js
@@ -1,4 +1,4 @@
-/*import React, { Component } from 'react'
+import React, { Component } from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import {Link} from 'react-router-dom'
--- src/screens/Results/Results.js
+++ src/screens/Results/Results.js
@@ -1,58 +1,61 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Busqueda from '../../components/Busqueda/Busqueda';
 import Card from '../../components/Card/Card';
 
 const API = "b4012469dde0367276c9701f8ecc44fe"
 class Results extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      datos: [], 
-      
+      datos: [],
     }
-    
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const busqueda = this.props.match.params.buscado
     const mediaType = this.props.match.params.tipo
 
-  if(mediaType=="pelicula"){
+    if (mediaType == "pelicula") {
       fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API}&query=${busqueda}`)
-      
-            .then(response => response.json())
-            .then(data => this.setState({ datos: data.results }))
-            .catch(error => console.log(error)) }
 
-    else{
-    fetch(`https://api.themoviedb.org/3/search/tv?api_key=` + API +`&query=${busqueda}`)
-      
-            .then(response => response.json())
-            .then(data => this.setState({ datos: data.results }))
-            .catch(error => console.log(error)) 
-    }}
+        .then(response => response.json())
+        .then(data => this.setState({ datos: data.results }))
+        .catch(error => console.log(error))
+    }
+
+    else {
+      fetch(`https://api.themoviedb.org/3/search/tv?api_key=` + API + `&query=${busqueda}`)
+
+        .then(response => response.json())
+        .then(data => this.setState({ datos: data.results }))
+        .catch(error => console.log(error))
+    }
+  }
 
   render() {
     const busqueda = this.props.match.params.buscado
     const mediaType = this.props.match.params.tipo
     return (
-      
+
       <React.Fragment>
-        <Header/>
-         <h2 className="alert alert-warning">Resultados</h2>
-        {this.state.datos.map((pelicula,idx)=> (
-                        <Card 
-                            key= {pelicula+idx}
-                            titulo={mediaType=="pelicula"?pelicula.title: pelicula.name}
-                            id={pelicula.id}
-                            imagen={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
-                            type= {mediaType=="pelicula"? "movie":"serie"
-                            }
-                           
-                            descripcion={pelicula.overview} />))}
-    
-        <Footer/>
+        <Header />
+        <Busqueda />
+
+        <h2 className="alert alert-warning">Resultados</h2>
+        {this.state.datos.map((pelicula, idx) => (
+          <Card
+            key={pelicula + idx}
+            titulo={mediaType == "pelicula" ? pelicula.title : pelicula.name}
+            id={pelicula.id}
+            imagen={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
+            type={mediaType == "pelicula" ? "movie" : "serie"
+            }
+
+            descripcion={pelicula.overview} />))}
+
+        <Footer />
       </React.Fragment>
     )
   }
